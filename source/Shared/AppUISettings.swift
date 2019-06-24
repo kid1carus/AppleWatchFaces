eec747dd09faa74eafd46e71140016c995c39c63
Michael Hill <michaelhill@gmail.com>
2019-06-24 19:44:06 -0400

    adding files needed for numbers face

--- source/Shared/AppUISettings.swift
+++ source/Shared/AppUISettings.swift
@@ -47,7 +47,7 @@ class AppUISettings: NSObject {
         "80sDigital.jpg", "brass.jpg","brushedsteel.jpg","light-wood.jpg", "vinylAlbum.jpg", "watchGears.jpg",
         "copper.jpg","watchInnards.jpg","testPattern.jpg","tealCircuits.jpg","CYRings.jpg","orangeBlueAbstract.jpg","FireAndIce.jpg","retroSunset.jpg",
         "geometric.jpg","metro.jpg","glitch.jpg", "moonBackground.jpg","8bitSpacey.jpg", "mars.jpg", "space-background.jpg", "sunset80.jpg", "AppleCasioA168OG.png", "AppleCasioA168Gold.png",
-        "CasioF91OG.png", "CasioF91Gold.png", "CasioF91White.png"
+        "CasioF91OG.png", "CasioF91Gold.png", "CasioF91White.png", "NumbersFace.png"
         ]
     
     static func materialIsColor( materialName: String ) -> Bool {
